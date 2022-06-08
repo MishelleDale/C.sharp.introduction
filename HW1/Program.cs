50cd3e0b6b6886a7841f5b7a421a59746f7242fd
Ekaterina <mishelledale@gmail.com>
2022-06-08 22:48:48 +0300

    The first task: find the biggest on 2 numbers is done

---
+++ HW1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Выясняем какое из двух чисел больше"); //Задаем условие задачи
+Console.WriteLine("Введите первое число");
+
+int num1 = Convert.ToInt32(Console.ReadLine()); //Преобразуем число в систему int
+
+Console.WriteLine("Введите второе число");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) // Сравниваем два числа через if-else
+    {
+    Console.Write(num1);
+    Console.Write(" больше ");
+    Console.WriteLine(num2);
+    }
+else
+   { 
+    Console.Write(num2);
+    Console.Write(" больше ");
+    Console.WriteLine(num1);
+    }
