54f801dedb2339daf00185a00588f7e79c940d29
Ekaterina <mishelledale@gmail.com>
2022-06-09 22:34:58 +0300

    Task4: Find all even number from 1 to N is completed

---
+++ HW4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Выстаиваем последовательность четных чисел от 1 до N");
+Console.WriteLine("Введите число N");
+
+int NumN = Convert.ToInt32(Console.ReadLine());
+if (NumN < 1) 
+    Console.WriteLine("Введите число от 1");
+
+int index = 1;
+
+while (index <= NumN)
+{
+    if (index % 2 == 0)
+        Console.Write(index + ", ");
+    index++;
+}
