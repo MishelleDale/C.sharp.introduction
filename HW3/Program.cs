b783b2def98356ecaaf3623c6601471825ba4c13
Ekaterina <mishelledale@gmail.com>
2022-06-09 22:05:13 +0300

    Task3: Find if a number is even is completed

---
+++ HW3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Проверяем, является ли число четным");
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number%2 == 0)
+    Console.Write(Number + " является четным числом");
+else
+    Console.Write(Number + " не является четным числом");
